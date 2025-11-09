3d0dc2b9f2cac1f5947afb9d2c7503ef7e26d3a2
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-09 16:17:49 +1300

    T

--- variables.tf
+++ variables.tf
@@ -4,6 +4,10 @@ variable "ami" {
 
 variable "instance_type" {
   type = string
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.large"], var.instance_type)
+    error_message = "Invalid instance type. Must be one of: t2.micro, t3.small, m5.large."
+  }
 }
 
 variable "sg_id" {
